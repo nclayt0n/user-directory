520dcda957a8e7318aabcb46100e569ec4ec2461
Nicole Clayton <50276082+nclayt0n@users.noreply.github.com>
2019-12-05 21:52:02 -0500

    adding buttons to userlist

--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -9,37 +9,43 @@ class UserList extends React.Component{
             page:1,
             pageLimit:10,
             users:[]
-  }
-}
+        }
+    }   
     componentDidMount(){
-        const options={
-            method:'GET',
-            headers:{
-                'content-type':'application/json'
-            },
-        };
-            Promise.all([
-                fetch(`${config.API_ENDPOINT}`,
-          options)
-            ])
-            .then(([usersRes]) => {
-                console.log(usersRes)
-                if (!usersRes.ok)
-                    return usersRes.json().then(e => Promise.reject(e));
-                    return Promise.all([usersRes.json()]);
-            })
-            .then(([users]) => {
-               this.setState({users:users.results})
-            })
-            .catch(error => {
-                console.error({ error });
-            });
+        // const options={
+        //     method:'GET',
+        //     headers:{
+        //         'content-type':'application/json'
+        //     },
+        // };
+        //     Promise.all([
+        //         fetch(`${config.API_ENDPOINT}`,
+        //   options)
+        //     ])
+        //     .then(([usersRes]) => {
+        //         if (!usersRes.ok)
+        //             return usersRes.json().then(e => Promise.reject(e));
+        //             return Promise.all([usersRes.json()]);
+        //     })
+        //     .then(([users]) => {
+        //        this.setState({users:users.results})
+        //     })
+        //     .catch(error => {
+        //         console.error({ error });
+        //     });
     }
     render(){
-        console.log(this.state)
+        let usersDisplayed=this.state.users.slice(this.state.page*10-10,this.state.page*10);
+        console.log(this.state.page)
       return(
           <div className='userList'>
              <UserCard/>
+            {this.state.page===1?null: <button type='button' onClick={()=>this.setState({page:this.state.page-1})}>Back</button>}
+            {this.state.page===1?null: <button type='button'>{this.state.page-1}</button>}
+            <button type='button'>{this.state.page}</button>
+            <button type='button'>{this.state.page+1}</button>
+      <button type='button' onClick={()=>this.setState({page:this.state.page+1})}>Next</button>
+
           </div>
       )  
     }
